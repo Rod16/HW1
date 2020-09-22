197713f36679f74f6f9049260dac83770efbf976
Rod16 <54881317+Rod16@users.noreply.github.com>
2020-09-22 15:21:52 +0300

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include "calculator.h"
+
+int Calculator::Add (int a, int b)
+{
+    return a + b;
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+//.randomizer_page {
+    //background: none;
+    //border: 2px solid #9FFF00;
+    //border-radius: 100px;
+    //color: #9FFF00;
+    //text-align: center;
+    //margin-left: 35%;
+    //margin-right: 35%;
+//}
